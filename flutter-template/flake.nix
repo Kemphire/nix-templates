95a3097d8466483db173fdbb27151e1408da93cc
Kartikey Shahi <kartikeyshahi@hotmail.com>
2025-08-03 19:07:56 +0530

    added some more settings in flutter template

--- flutter-template/flake.nix
+++ flutter-template/flake.nix
@@ -17,17 +17,40 @@
           allowUnfree = true;
         };
       };
+      nixpkgs.config = {
+        android_sdk.accept_license = true;
+        allowUnfree = true;
+      };
       buildToolsVersion = "34.0.0";
       androidComposition = pkgs.androidenv.composeAndroidPackages {
         buildToolsVersions = [buildToolsVersion "28.0.3"];
         platformVersions = ["34" "28"];
-        abiVersions = ["armeabi-v7a" "arm64-v8a"];
+        abiVersions = ["armeabi-v7a" "arm64-v8a" "x86_64"];
+        includeEmulator = true;
+        emulatorVersion = "35.1.4";
+        includeSystemImages = true;
+        systemImageTypes = ["google_apis_playstore"];
+        includeSources = false;
+        extraLicenses = [
+          "android-googletv-license"
+          "android-sdk-arm-dbt-license"
+          "android-sdk-license"
+          "android-sdk-preview-license"
+          "google-gdk-license"
+          "intel-android-extra-license"
+          "intel-android-sysimage-license"
+          "mips-android-sysimage-license"
+        ];
       };
       androidSdk = androidComposition.androidsdk;
     in {
       devShell = with pkgs;
         mkShell {
           ANDROID_SDK_ROOT = "${androidSdk}/libexec/android-sdk";
+          CHROME_EXECUTABLE = "${pkgs.google-chrome}/bin/chrome";
+          ANDROID_HOME = "${androidSdk}/libexec/android-sdk";
+          JAVA_HOME = "${pkgs.jdk17}";
+          GRADLE_OPTS = "-Dorg.gradle.project.android.aapt2FromMavenOverride=${androidSdk}/libexec/android-sdk/build-tools/34.0.0/aapt2";
           buildInputs = [
             flutter
             androidSdk # The customized SDK that we've made above
