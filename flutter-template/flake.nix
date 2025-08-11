596037b751d537fc7f372bfca6ed80c9df17dfbc
Kartikey Shahi <kartikeyshahi@hotmail.com>
2025-08-12 02:04:54 +0530

    some more changes

--- flutter-template/flake.nix
+++ flutter-template/flake.nix
@@ -20,15 +20,30 @@
         };
 
         androidEnv = pkgs.androidenv.override {licenseAccepted = true;};
+
         androidComposition = androidEnv.composeAndroidPackages {
           cmdLineToolsVersion = "8.0";
           platformToolsVersion = "34.0.4";
-          buildToolsVersions = ["30.0.3" "33.0.2" "34.0.0"];
-          platformVersions = ["28" "31" "32" "33" "34"];
+          buildToolsVersions = [
+            "30.0.3"
+            "33.0.2"
+            "34.0.0"
+          ];
+          platformVersions = [
+            "28"
+            "31"
+            "32"
+            "33"
+            "34"
+            "35"
+          ];
           abiVersions = ["x86_64"];
           includeNDK = true;
           includeSystemImages = true;
-          systemImageTypes = ["google_apis" "google_apis_playstore"];
+          systemImageTypes = [
+            "google_apis"
+            "google_apis_playstore"
+          ];
           includeEmulator = true;
           useGoogleAPIs = true;
           extraLicenses = [
@@ -45,54 +60,68 @@
 
         androidSdk = androidComposition.androidsdk;
 
-        fhs =
-          pkgs.buildFHSEnv
-          {
-            name = "fhs";
+        fhs = pkgs.buildFHSEnv {
+          name = "fhs";
 
-            targetPkgs = pkgs:
-              with pkgs; [
-                androidSdk
-                flutter
-                qemu_kvm
-                gradle
-                jdk17
-                google-chrome
-                vulkan-loader
-                libGL
-                mesa-demos
-                fish
-              ];
+          targetPkgs = pkgs:
+            with pkgs; [
+              androidSdk
+              flutter
+              qemu_kvm
+              gradle
+              jdk17
+              google-chrome
+              vulkan-loader
+              libGL
+              mesa-demos
+              fish
+            ];
 
-            # Runs every time you enter the env
-            profile = ''
-              export ANDROID_HOME="${androidSdk}/libexec/android-sdk"
-              export ANDROID_SDK_ROOT="${androidSdk}/libexec/android-sdk"
-              export JAVA_HOME="${pkgs.jdk17.home}"
-              export CHROME_EXECUTABLE="${pkgs.google-chrome}/bin/google-chrome-stable"
-              export FLUTTER_ROOT="${pkgs.flutter}"
-              export DART_ROOT="${pkgs.flutter}/bin/cache/dart-sdk"
-              export GRADLE_OPTS="-Dorg.gradle.project.android.aapt2FromMavenOverride=${androidSdk}/libexec/android-sdk/build-tools/33.0.2/aapt2"
-              export QT_QPA_PLATFORM="wayland;xcb"
-              export LD_LIBRARY_PATH="${pkgs.lib.makeLibraryPath [pkgs.vulkan-loader pkgs.libGL]}:$LD_LIBRARY_PATH"
+          # Runs every time you enter the env
+          profile = ''
+            if [ -d "$HOME/android-sdk" ]; then
+              rm -rf "$HOME/android-sdk"
+              cp -rL --no-preserve=ownership "${androidSdk}/libexec/android-sdk" "$HOME/android-sdk"
+              chmod -R u+rw "$HOME/android-sdk"
+              export ANDROID_HOME="$HOME/android-sdk"
+              export ANDROID_SDK_ROOT="$HOME/android-sdk"
+            else
+              cp -rL --no-preserve=ownership "${androidSdk}/libexec/android-sdk" "$HOME/android-sdk"
+              chmod -R u+rw "$HOME/android-sdk"
+              export ANDROID_HOME="$HOME/android-sdk"
+              export ANDROID_SDK_ROOT="$HOME/android-sdk"
+            fi
 
-              # Add Dart pub global binaries to PATH
-              if [ -n "$PUB_CACHE" ]; then
-                export PATH="$PATH:$PUB_CACHE/bin"
-              else
-                export PATH="$PATH:$HOME/.pub-cache/bin"
-              fi
-            '';
+            export JAVA_HOME="${pkgs.jdk17.home}"
+            export CHROME_EXECUTABLE="${pkgs.google-chrome}/bin/google-chrome-stable"
+            export FLUTTER_ROOT="${pkgs.flutter}"
+            export DART_ROOT="${pkgs.flutter}/bin/cache/dart-sdk"
+            export GRADLE_OPTS="-Dorg.gradle.project.android.aapt2FromMavenOverride=${androidSdk}/libexec/android-sdk/build-tools/33.0.2/aapt2"
+            export QT_QPA_PLATFORM="wayland;xcb"
+            export LD_LIBRARY_PATH="${
+              pkgs.lib.makeLibraryPath [
+                pkgs.vulkan-loader
+                pkgs.libGL
+              ]
+            }:$LD_LIBRARY_PATH"
 
-            runScript = "fish";
-          };
+            # Add Dart pub global binaries to PATH
+            if [ -n "$PUB_CACHE" ]; then
+              export PATH="$PATH:$PUB_CACHE/bin"
+            else
+              export PATH="$PATH:$HOME/.pub-cache/bin"
+            fi
+          '';
+
+          runScript = "fish";
+        };
       in {
         devShell = pkgs.mkShell {
           buildInputs = [fhs];
         };
 
         shellHook = ''
-          echo  "You are entering to a nix flutter environment, hurray"
+          echo "You are entering to a nix flutter environment, hurray"
           fhs
         '';
       }
